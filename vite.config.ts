960eb11dfaf879aadc104692cbfafecf98ef039f
uxDimon <bobyorrr@gmail.com>
2023-06-12 00:31:36 +0300

    + babylonjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default defineConfig(() => {
+	return {
+		build: {
+			target: "esnext",
+		},
+		// esbuild: {
+		// 	supported: {
+		// 		"top-level-await": true, //browsers can handle top-level-await features
+		// 	},
+		// },
+		resolve: {
+			alias: {
+				"@modules": resolve(__dirname, "src/modules"),
+				"@ui": resolve(__dirname, "src/ui"),
+			},
+		},
+		plugins: [svelte()],
+	};
+});
