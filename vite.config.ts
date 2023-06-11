4b5f1f3cf91993ec78e22ba157a497f839b187cf
uxDimon <bobyorrr@gmail.com>
2023-06-12 00:31:42 +0300

    --

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	return {
-		build: {
-			target: "esnext",
-		},
-		// esbuild: {
-		// 	supported: {
-		// 		"top-level-await": true, //browsers can handle top-level-await features
-		// 	},
-		// },
 		resolve: {
 			alias: {
 				"@modules": resolve(__dirname, "src/modules"),
